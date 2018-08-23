33e7cd6cbb3ae37f06b1f06e9d5d565dd0c830f1
Ronaldo <kikogamer@gmail.com>
2018-08-22 23:04:07 -0300

    created controllers talks and team_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'channels/create'
-
-  get 'channels/destroy'
-
-  get 'channels/show'
-
+  root to: 'teams#index'
+  resources :teams, only: [:create, :destroy]
+  get '/:slug', to: 'teams#show'
+  resources :channels, only: [:show, :create, :destroy]
+  resources :talks, only: [:show]
+  resources :team_users, only: [:create, :destroy]
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
