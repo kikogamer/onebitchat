4486b7187e20d9ef0ca739e8a3cdc8a1933be4ff
Ronaldo <kikogamer@gmail.com>
2018-08-26 18:23:37 -0300

    action cable config

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :channels, only: [:show, :create, :destroy]
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
+  mount ActionCable.server => '/cable'
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
