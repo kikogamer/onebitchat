cd106857e6908445048b66f52485b36242cf1c18
Ronaldo <kikogamer@gmail.com>
2018-09-23 07:46:20 -0300

    add invitation on add user, configured devise-invitable and configured devise mailer

--- app/assets/javascripts/team_users/add.coffee
+++ app/assets/javascripts/team_users/add.coffee
@@ -1,7 +1,7 @@
 $(document).on 'turbolinks:load', ->
   $(".add_user").on 'click', (e) =>
     $('#add_user_modal').modal('open')
-    $('#team_user_team_id').val(e.target.id)
+    $('#invitation_team_id').val(e.target.id)
     return false
 
   $('.add_user_form').on 'submit', (e) ->
@@ -9,14 +9,14 @@ $(document).on 'turbolinks:load', ->
         type: 'POST'
         dataType: 'json',
         data: {
-          team_user: {
-            email: $('#team_user_email').val()
-            team_id: $('#team_user_team_id').val()
+          invitation: {
+            name: $('#invitation_name').val()
+            email: $('#invitation_email').val()
+            team_id: $('#invitation_team_id').val()
           }
         }
         success: (data, text, jqXHR) ->
-          window.add(data['user']['name'], data['user']['id'], 'user')
-          Materialize.toast('Success in add User &nbsp;<b>:(</b>', 4000, 'green')
+          Materialize.toast('User was invited to join the team &nbsp;<b>:)</b>', 4000, 'green')
         error: (jqXHR, textStatus, errorThrown) ->
           Materialize.toast('Problem in add User &nbsp;<b>:(</b>', 4000, 'red')
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,13 +26,22 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Set default host for mailers, remember to do this in production as well!
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: ENV["MAIL_FROM"] } 
+  config.action_mailer.smtp_settings = {
+    address:              ENV["MAIL_ADDRESS"],
+    port:                 587,
+    domain:               ENV["MAIL_DOMAIN"],
+    user_name:            ENV["MAIL_USERNAME"],                                                                                                                   
+    password:             ENV["MAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
